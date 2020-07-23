9c9461ca358e9d09e6d11eeda41fc379c58029c3
ToF- <cthibaut@octo.com>
2020-07-23 22:24:57 +0200

    added tests

--- Forth/Tests.fs
+++ Forth/Tests.fs
@@ -1,12 +1,16 @@
 REQUIRE ffl/tst.fs
 REQUIRE Cyatq.fs
-50000 7 * CONSTANT MAX-LENGTH
 
-: READ-NUMBERS
-    INPUT-LINE DUP MAX-LENGTH STDIN READ-LINE THROW 
-    IF EVALUATE ELSE 2DROP THEN ;    
 
-READ-NUMBERS 
-CR
-.S
+T{ ." GET-NUMBER reads the next number from a string " CR
+    S" 4807 42 17 " DROP 
+    GET-NUMBER 4807 ?S DROP }T
+
+CREATE MY-NUMBERS 3 CELLS ALLOT
+T{ ." GET-NUMBERS reads N numbers from a string and store them " CR
+    S" 4807 42 17 " DROP
+    MY-NUMBERS 3 GET-NUMBERS 
+    MY-NUMBERS 0 CELLS + @ 4807 ?S
+    MY-NUMBERS 1 CELLS + @   42 ?S
+    MY-NUMBERS 2 CELLS + @   17 ?S }T
 BYE

39abe39dac8663308809fd8bcf8b70c352fa5287
ToF- <cthibaut@octo.com>
2020-07-24 14:17:17 +0200

    build a sum tree

--- Forth/Cyatq.fs
+++ Forth/Cyatq.fs
@@ -41,3 +41,47 @@ CREATE NUMBERS MAXIMUM-NUMBER CELLS ALLOT
         DUP I CELLS + @ . 
     LOOP DROP ;
 
+: MIDDLE ( l,r -- m )
+    OVER - + ; 
+
+: FIRST-LEAF-POSITION ( n -- p )
+    1 SWAP
+    BEGIN
+        DUP 0> WHILE
+            2/ SWAP 2* SWAP
+    REPEAT DROP ;
+
+: BUILD-NODE ( t,p -- )
+    OVER OVER \ t,p,t,p
+    2* CELLS + \ t,p,tp*2
+    DUP CELL+  \ t,p,tp*2,tp*2+1
+    @ SWAP @ + \ t,p,v
+    -ROT CELLS + ! ;  \ t[p]<-v
+    
+    
+: BUILD-LEAVES ( t,a,n -- )
+    DUP >R FIRST-LEAF-POSITION \ t,a,p
+    CELLS ROT + SWAP           \ tp,a
+    R> 0 DO 
+        OVER OVER           \ tp,a,tp,a
+        @ SWAP !            \ tp,a -- tp <- [a] 
+        CELL+ SWAP          \ a+1,tp
+        CELL+ SWAP          \ tp+1,a+1
+    LOOP 2DROP ;
+
+: BUILD-NODE-LEVEL ( t,p -- )
+    DUP 2/ DO
+        DUP I 2* CELLS +  \ t,t2i
+        DUP CELL+         \ t,t2i,t2i+1
+        @ SWAP @ +        \ t,v
+        OVER I CELLS + !  \ t -- t[i] <- v
+    LOOP DROP ;
+
+: BUILD-NODES ( t p -- )
+    BEGIN
+        ?DUP WHILE 
+        2DUP BUILD-NODE-LEVEL
+        2/
+    REPEAT DROP ;
+
+    
--- Forth/Tests.fs
+++ Forth/Tests.fs
@@ -1,6 +1,8 @@
 REQUIRE ffl/tst.fs
 REQUIRE Cyatq.fs
 
+CREATE MY-NUMBERS 10 CELLS ALLOT
+CREATE MY-TREE    40 CELLS ALLOT
 
 T{ ." GET-NUMBER reads the next number from a string " CR
     S" 4807 42 17 " DROP 
@@ -10,11 +12,61 @@ T{ ." GET-NUMBER reads the next negative number from a string " CR
     S" -4807 42 17 " DROP 
     GET-NUMBER -4807 ?S DROP }T
 
-CREATE MY-NUMBERS 3 CELLS ALLOT
+
 T{ ." GET-NUMBERS reads N numbers from a string and store them " CR
     S" 4807 -42 17 " DROP
     MY-NUMBERS 3 GET-NUMBERS 
     MY-NUMBERS 0 CELLS + @ 4807 ?S
     MY-NUMBERS 1 CELLS + @  -42 ?S
     MY-NUMBERS 2 CELLS + @   17 ?S }T
+
+T{ ." BUILD-LEAVES builds the leaves of sum tree from an array " CR
+    S" 42 17 4807 23 -5 1 -100 52 1000 -500 " DROP
+    MY-NUMBERS 10 GET-NUMBERS
+    MY-TREE MY-NUMBERS 10 BUILD-LEAVES
+    MY-TREE 10 FIRST-LEAF-POSITION CELLS + 
+    DUP       @ 42 ?S
+    CELL+ DUP @ 17 ?S
+    CELL+ DUP @ 4807 ?S
+    CELL+ DUP @ 23 ?S
+    CELL+ DUP @ -5 ?S
+    CELL+ DUP @ 1 ?S
+    CELL+ DUP @ -100 ?S
+    CELL+ DUP @ 52 ?S
+    CELL+ DUP @ 1000 ?S
+    CELL+ DUP @ -500 ?S
+    DROP
+}T
+
+T{ ." BUILD-NODES builds the nodes of the sum tree " CR
+    MY-TREE 10 FIRST-LEAF-POSITION BUILD-NODES
+    MY-TREE 
+    CELL+ DUP @   5337 ?S   \ 4837+500
+    CELL+ DUP @   4837 ?S   \ 4889-52
+    CELL+ DUP @    500 ?S   \ 500+0
+    CELL+ DUP @   4889 ?S   \ 4830+59
+    CELL+ DUP @    -52 ?S   \ -4-48
+    CELL+ DUP @    500 ?S   \ 500+0
+    CELL+ DUP @      0 ?S   \ 0/0
+    CELL+ DUP @     59 ?S   \ 42+17
+    CELL+ DUP @   4830 ?S   \ 4807+23
+    CELL+ DUP @     -4 ?S   \ -5+1
+    CELL+ DUP @    -48 ?S   \ -100+52
+    CELL+ DUP @    500 ?S   \ 1000-500
+    CELL+ DUP @      0 ?S
+    CELL+ DUP @      0 ?S
+    CELL+ DUP @      0 ?S
+    CELL+ DUP @     42 ?S
+    CELL+ DUP @     17 ?S
+    CELL+ DUP @   4807 ?S
+    CELL+ DUP @     23 ?S
+    CELL+ DUP @     -5 ?S
+    CELL+ DUP @      1 ?S
+    CELL+ DUP @   -100 ?S
+    CELL+ DUP @     52 ?S
+    CELL+ DUP @   1000 ?S
+    CELL+ DUP @   -500 ?S
+    DROP
+}T
 BYE
+

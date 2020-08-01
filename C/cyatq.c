d99b9b6c603bc0a7eb92d057cc39481d9cacae50
Christophe Thibaut <cthibaut@octo.com>
2020-08-01 14:13:41 +0200

    refactoring

--- C/cyatq.c
+++ C/cyatq.c
@@ -7,7 +7,8 @@
 #define MAXNUMBER 50000
 #define LEFT(p)  (p*2)
 #define RIGHT(p) (p*2+1)
-#define INT_MIN (-999999999)
+#define INT_MIN (-999999)
+// #define DEBUG (1)
 
 long Numbers[MAXNUMBER];
 char Line[MAXLINE];
@@ -18,7 +19,6 @@ struct node {
     long max_suffix_sum;
     long max_segment_sum;
 } SegmentTree[MAXNUMBER*4];
-
 
 int get_int(char *line) {
     int n;
@@ -45,68 +45,97 @@ int max(int a, int b) {
 }
 
 void print_node(struct node n) {
-    printf("ss:%ld mpr:%ld msu:%ld mss:%ld\n", n.segment_sum, n.max_prefix_sum, n.max_suffix_sum, n.max_segment_sum);
+    printf("ss:%ld mpr:%ld msu:%ld mss:%ld\n", 
+            n.segment_sum, 
+            n.max_prefix_sum, 
+            n.max_suffix_sum, 
+            n.max_segment_sum);
 }
-void build_tree(int l, int r, long *numbers, struct node *tree, int p) {
+
+struct node merge_nodes(struct node left, struct node right) {
+    struct node target;
+    target.segment_sum = left.segment_sum + right.segment_sum;
+
+    target.max_prefix_sum = 
+        max ( left.max_prefix_sum,  
+                left.segment_sum + right.max_prefix_sum );
+
+    target.max_suffix_sum = 
+        max ( right.max_suffix_sum, 
+                right.segment_sum + left.max_suffix_sum );
+
+    target.max_segment_sum = 
+        max ( left.max_segment_sum,
+                max ( right.max_segment_sum, 
+                    left.max_suffix_sum + right.max_prefix_sum )); 
+    return target;
+}
+
+struct node build_tree(int l, int r, long *numbers, struct node *tree, int p) {
     if (l == r) {
         int v = numbers[l-1];
         tree[p].segment_sum     = v;
         tree[p].max_prefix_sum  = v;
         tree[p].max_suffix_sum  = v;
         tree[p].max_segment_sum = v;
+#if DEBUG
+        printf("built "); print_node(tree[p]);
+#endif
+        return tree[p];
     } 
     else {
         int m = (l + r) / 2;
-        build_tree(l, m, numbers, tree, LEFT(p));
-        build_tree(m+1,r,numbers, tree, RIGHT(p));
-        struct node left  = tree[LEFT(p)];
-        struct node right = tree[RIGHT(p)];
-        tree[p].segment_sum = left.segment_sum + right.segment_sum;
-        tree[p].max_prefix_sum = max(left.max_prefix_sum,  left.segment_sum + right.max_prefix_sum);
-        tree[p].max_suffix_sum = max(right.max_suffix_sum, right.segment_sum + left.max_suffix_sum);
-        tree[p].max_segment_sum = max(left.max_segment_sum,max(right.max_segment_sum, left.max_suffix_sum + right.max_prefix_sum)); 
+        struct node left = build_tree(l, m, numbers, tree, LEFT(p));
+        struct node right= build_tree(m+1,r,numbers, tree, RIGHT(p));
+        tree[p] = merge_nodes(left, right);
+#if DEBUG
+        printf("built "); print_node(tree[p]);
+#endif
+        return tree[p];
     }
 }
 
 struct node query_tree(int l, int r, int x, int y, struct node *tree, int p) {
-    // printf("looking for [%d %d] in [ %d %d]\n", x, y, l, r);
+#if DEBUG
+    printf("looking for [%d %d] in [ %d %d]\n", x, y, l, r);
+#endif
     if (x > r || y < l) {
-        // printf("impossible\n");
+#if DEBUG
+        printf("impossible\n");
+#endif
         struct node result; 
         result.segment_sum     = INT_MIN;
         result.max_prefix_sum  = INT_MIN;
         result.max_suffix_sum  = INT_MIN;
         result.max_segment_sum = INT_MIN;
-        // print_node(result);
+#if DEBUG
+        print_node(result);
+#endif
         return result;
     }
     if (l >= x && r <= y) {
-        // printf("found ");
-        // print_node(tree[p]);
+#if DEBUG
+        printf("found ");
+        print_node(tree[p]);
+#endif
         return tree[p];
     }
-    else {
-        int m = (l + r) / 2;
-        struct node left = query_tree(l, m, x, y, tree, LEFT(p));
-        struct node right= query_tree(m+1,r,x, y, tree, RIGHT(p));
-        struct node result;
-        result.segment_sum = left.segment_sum + right.segment_sum;
-        result.max_prefix_sum = max(left.max_prefix_sum, left.segment_sum + right.max_prefix_sum);
-        result.max_suffix_sum = max(right.max_suffix_sum, right.segment_sum + left.max_suffix_sum);
-        result.max_segment_sum = max(left.max_segment_sum, max(right.max_segment_sum, 
-                                     left.max_suffix_sum + right.max_prefix_sum));
-        // printf("merging ");
-        // print_node(left);
-        // print_node(right);
-        // print_node(result);
-        return result;
-    }
+    int m = (l + r) / 2;
+    struct node left = query_tree(l, m, x, y, tree, LEFT(p));
+    struct node right= query_tree(m+1,r,x, y, tree, RIGHT(p));
+    struct node result = merge_nodes(left, right);
+#if DEBUG
+    printf("merging "); print_node(left);
+    printf("with    "); print_node(right);
+    printf("result= ");print_node(result);
+#endif
+    return result;
 }
 void print_numbers(int *numbers, int n) {
     for (int i=0; i<n; i++) {
         printf("%d ",numbers[i]);
     }
-     printf("\n");
+    printf("\n");
 }
 int main() {
     int max_number = get_int(Line);
